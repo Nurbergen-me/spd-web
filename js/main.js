c8b36f975b957c431ce8be101223381a22e3614e
nurik.eleshov <nurik.eleshov@gmail.com>
2023-02-14 09:45:26 +0600

    hidden menu

--- js/main.js
+++ js/main.js
@@ -40,3 +40,25 @@ slider.addEventListener('mousemove', (e) => {
     const walk = (x - startX) * 1; //scroll-fast
     slider.scrollLeft = scrollLeft - walk;
 });
+
+
+// Burger toggle
+
+var burger = document.querySelector('.navbar__burger')
+var burgerClose = document.querySelector('.hidden__close')
+var hiddenMenu =  document.querySelector('.hidden')
+var hiddenLinks = document.querySelectorAll('.hidden__link')
+
+burger.addEventListener('click', () => {
+    hiddenMenu.classList.toggle('active')
+})
+
+burgerClose.addEventListener('click', () => {
+    hiddenMenu.classList.remove('active')
+})
+
+hiddenLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hiddenMenu.classList.remove('active')
+    })
+})
